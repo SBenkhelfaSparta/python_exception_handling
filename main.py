080d7e7780fbb69ae8e1a38c8c4bf03851b237df
unknown <sbenkhelfa@spartaglobal.com>
2021-06-30 12:35:20 +0100

    task last update

--- main.py
+++ main.py
@@ -16,4 +16,4 @@
 
 from pack import p_exceptions
 pex = p_exceptions.P_Exceptions()
-pex.exce()
+pex.exce(input("Please enter what item you would like to add: "))
--- pack/p_exceptions.py
+++ pack/p_exceptions.py
@@ -1,12 +1,15 @@
 class P_Exceptions:
-    def exce(self):
+    def exce(self, item):
         try:
-            file = open("order.txt")
+            file = open("order.txt", "a")
             print("File found")
+            file.write(item+"\n")
+            file.close()
         except FileNotFoundError as errmsg:
             print("File not found: {}".format(errmsg))
+            print("Creating the file...")
             file2 = open("order.txt", "w")
-            file2.write("one of everything")
+            file2.write(item+"\n")
             file2.close()
         finally:
             print('Goodbye')
